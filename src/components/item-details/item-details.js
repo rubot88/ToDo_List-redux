1b809aef79636c4d43aef2b4e79d436f9e8b5ffe
Rustam Botirov <rustambotirov88@gmail.com>
2019-12-15 17:19:59 +0200

    mark items feature is added

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -3,20 +3,33 @@ import PropTypes from 'prop-types';
 
 import './item-details.scss';
 
-const ItemDetails = ({ label }) => {
+const ItemDetails = ({ label, isCompleted, isImportant, onItemComplete, onImportant, onDelete }) => {
+    let itemClass = 'label';
+    if (isCompleted) itemClass += ' completed';
+    if (isImportant) itemClass += ' important';
     return (
         <div className="item-details">
-            <div className="label">{label}</div>
+            <div className={itemClass}
+                onClick={onItemComplete}>{label}</div>
             <div className="buttons">
-                <button className="btn btn-outline-danger btn-sm">del</button>
-                <button className="btn btn-outline-success btn-sm">!</button>
+                <button
+                    className="btn btn-outline-danger btn-sm"
+                    onClick={onDelete}>del</button>
+                <button
+                    className="btn btn-outline-success btn-sm"
+                    onClick={onImportant}>!</button>
             </div>
-        </div>
+        </div >
     );
 };
 
 ItemDetails.propTypes = {
-    label: PropTypes.string
+    label: PropTypes.string,
+    isCompleted: PropTypes.bool,
+    isImportant: PropTypes.bool,
+    onItemComplete: PropTypes.func,
+    onImportant: PropTypes.func,
+    onDelete: PropTypes.func
 };
 
 export default ItemDetails;
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -8,19 +8,65 @@ import './item-list.scss';
 class componentName extends Component {
     state = {
         items: [
-            { id: 1, label: 'Drink Coffee' },
-            { id: 2, label: 'Learn React' },
-            { id: 3, label: 'Make Awesome App' }
+            { id: 1, label: 'Drink Coffee', complete: true, important: false },
+            { id: 2, label: 'Learn React', complete: false, important: false },
+            { id: 3, label: 'Make Awesome App', complete: false, important: false }
         ]
     }
+
+    onItemComplete = (id) => {
+        this.setState(({ items }) => {
+            const newItems = items
+                .map(item => item.id === id ?
+                    ({ ...item, complete: !item.complete }) :
+                    ({ ...item }));
+            return {
+                items: newItems
+            }
+        });
+    }
+
+    onImportant = (id) => {
+        this.setState(({ items }) => {
+            const newItems = items
+                .map(item => item.id === id ?
+                    ({ ...item, important: !item.important }) :
+                    ({ ...item }));
+            return {
+                items: newItems
+            }
+        })
+    }
+
+    onDelete = (id) => {
+        this.setState(({ items }) => {
+            const idx = items.findIndex(item => item.id === id);
+            const newItems = [
+                ...items.slice(0, idx),
+                ...items.slice(idx + 1)
+            ]
+
+            return {
+                items: newItems
+            }
+        })
+    }
+
     render() {
+
         const { items } = this.state;
-        const renderItems = items.map(({ id, label }) => {
+        const renderItems = items.map(({ id, label, complete, important }) => {
             return (
                 <li
                     className="item list-group-item"
                     key={id}>
-                    <ItemDetails label={label} />
+                    <ItemDetails
+                        label={label}
+                        isCompleted={complete}
+                        isImportant={important}
+                        onItemComplete={() => this.onItemComplete(id)}
+                        onImportant={() => this.onImportant(id)}
+                        onDelete={()=>this.onDelete(id)} />
                 </li>
             );
         });
